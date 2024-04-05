7dfca66153e15aa12118245e13e91d3b69b433b0
omnandurkar <nandurkarom172@gmail.com>
2024-04-06 01:41:30 +0530

    ref: minor changes

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,7 +3,8 @@ import Navbar from '../../components/Navbar/Navbar'
 import Features from '../../components/Features/Features'
 import AdvertiseVdo from '../../components/AdvertiseVdo/AdvertiseVdo.js'
 import Footer from '../../components/Footer/Footer.js'
-import Book2 from '../../components/Transport/Transport.js'
+// import Book2 from '../../components/Transport/Transport.js'
+import Transport from '../../components/Book2/Transport.js'
 
 function Home() {
   return (
