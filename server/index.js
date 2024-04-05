1a69f8fa3a510d4659813663c84e2b4b47328b70
omnandurkar <nandurkarom172@gmail.com>
2024-04-05 07:48:20 +0530

    Added basic server setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose, { connect } from 'mongoose';
+import dotenv  from 'dotenv';
+dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+// const connectDB = async () => {
+//     mongoose.connect(process.env.MONGODB_URL)
+//     console.log('Database Connected');
+// }
+
+// connectDB();
+
 
 const PORT = 5000;
- 
+
+app.get("/health", (req,res) =>{
+    res.json({
+        success : true,
+        message : "Server is Running",
+        data : null
+    })
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
