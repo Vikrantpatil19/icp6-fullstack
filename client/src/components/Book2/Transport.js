abac6c71ce0abf61589d687113ffa09bef057dcf
omnandurkar <nandurkarom172@gmail.com>
2024-04-06 02:22:38 +0530

    Added two booking

--- client/src/components/Book2/Transport.js
+++ client/src/components/Book2/Transport.js
@@ -1,27 +1,20 @@
-import React from 'react'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import bookingData from '../../configs/Booking/Transport.json';
 
 export default function Transport() {
     return (
-        <>
-            <div class="container col-xxl-8 px-4 py-5 ">
-                <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-                    <div class="col-10 col-sm-8 col-lg-6 border rounded-2 p-3">
-                        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 border rounded-2 p-3">
-
-                        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-                        </div>
+        <div className="container col-xxl-12 px-4 py-5 d-flex mx-auto">
+            {bookingData.map((block) => (
+                <div className="mx-auto" key={block.id}>
+                    <div className="col-10 col-sm-8 col-lg-8 border rounded-2 p-3">
+                        <h2>{block.title}</h2>
+                        <p>{block.description}</p>
+                        <Link to={`/booking/${block.id}`} className="btn btn-primary btn-lg px-4 me-md-2">{block.buttonText}</Link>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            ))}
+        </div>
+    );
 }
