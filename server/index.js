69a7728bbf0e5b20f734ed21acada91bf2ef0ddc
Vikrantpatil19 <pvikrant010@gmail.com>
2024-04-06 11:03:31 +0530

    Add the models folder for payment page

--- server/index.js
+++ server/index.js
@@ -4,16 +4,18 @@ import mongoose, { connect } from 'mongoose';
 import dotenv  from 'dotenv';
 dotenv.config();
 
+import Payment from './models/paymentpage_model.js';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const connectDB = async () => {
-//     mongoose.connect(process.env.MONGODB_URL)
-//     console.log('Database Connected');
-// }
+ const connectDB = async () => {
+     mongoose.connect(process.env.MONGODB_URL)
+     console.log('Database Connected');
+ }
 
-// connectDB();
+ connectDB();
 
 
 const PORT = 5000;
@@ -25,6 +27,16 @@ app.get("/health", (req,res) =>{
         data : null
     })
 });
+
+app.post("/payment" , (req , res) =>{
+
+    const {First_Name , Last_Name , Date_of_Birth , Phone_Number , Email , City , Zip  } = req.body;
+
+    const ticket = new Ticket({
+        name,
+        phoneNumber,
+    })
+})
 
 
 app.listen(PORT, () => {
